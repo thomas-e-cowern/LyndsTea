02b869801fa9a9f82a3d0159577dc6329440a690
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-31 16:10:54 -0400

    add main image and placed in ContentView

--- LyndsTea/ContentView.swift
+++ LyndsTea/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("main")
+                .resizable()
+                .scaledToFit()
         }
         .padding()
     }
