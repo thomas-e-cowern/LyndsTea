2d24fc45250bbfef2537f131e1f1a6b562b99517
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-31 08:56:01 -0400

    LyndsTea initial commit

---
+++ LyndsTea/LyndsTeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyndsTeaApp.swift
+//  LyndsTea
+//
+//  Created by Thomas Cowern on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct LyndsTeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
