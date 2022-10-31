31ffff67d7e7a04dce71aee2518f2ff4b079caa2
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-31 16:40:26 -0400

    add CommunityView

---
+++ LyndsTea/Views/CommunityView.swift
@@ -0,0 +1,20 @@
+//
+//  CommunityView.swift
+//  LyndsTea
+//
+//  Created by Thomas Cowern on 10/31/22.
+//
+
+import SwiftUI
+
+struct CommunityView: View {
+    var body: some View {
+        Text("Community View")
+    }
+}
+
+struct CommunityView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunityView()
+    }
+}
